0d9a4568eb5319d8fd963b4d0af9c0551ca46993
Finnik <73883383+golkity@users.noreply.github.com>
2025-09-23 09:42:09 +0300

    feat(cpp): add answer for second tasks

---
+++ B.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+
+double f_to_c(double f) {
+    return 5.0/9.0 * (f - 32.0);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int f;
+    std::cin>>f;
+
+    std::cout<<f_to_c(f)<<"\n";
+
+    return 0;
+}
